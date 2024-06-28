a98f274321a0939ef9e2d5b1933c2eb696917892
Georgios <158276611+GeorgiosVs@users.noreply.github.com>
2024-06-29 00:37:09 +0300

    C
    Guess the result in program C

---
+++ guess_result.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main (void){
+    int answer, result;
+
+    answer = 100;
+    result = answer - 10;
+
+    printf("Result is: %i\n",result + 5);
+
+    return 0;
+}
+//Program in C (Find what would the result be)
