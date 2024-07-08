baea2cf01e7946f55cdce1820267c800eaa40cf9
Georgios <158276611+GeorgiosVs@users.noreply.github.com>
2024-07-08 09:12:43 +0300

    C program that integrates values into a variables
    C program that integrates values into a variables also exports them into the terminal.

---
+++ c08_07_24.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+
+    int num;
+
+    char ch;
+    float f;
+    double d;
+
+    ch = 'X';
+    f = 100.123;
+    d = 123.009;
+
+    printf("\nch is %c, ", ch);
+    printf("\nf is %f, ", f);
+    printf("\nd is %f,", d);
+
+    return 0;
+}
