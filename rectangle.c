b1dd9af41bf5994b2a6cca59c2aae4c08a7daf3d
Georgios <158276611+GeorgiosVs@users.noreply.github.com>
2024-07-12 15:40:28 +0300

    area of ​​a rectangle with given dimensions

---
+++ rectangle.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(void){
+    int len, width;
+    
+    printf("Enter length: ");
+    scanf("%d", &len);
+    printf("Enter width: ");
+    scanf("%d", &width);
+    
+    
+    printf("Area is %d", len * width);
+    
+    return 0;
+}
