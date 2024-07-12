7bde5e130e1fc3236bff2d934d1d224fc91b597b
Georgios <158276611+GeorgiosVs@users.noreply.github.com>
2024-07-12 15:27:24 +0300

    floating point in C language

---
+++ floating_point.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(){
+    
+    int num;
+    float f;
+    
+    printf("\nEnter an integer : ");
+    scanf("%d", &num);
+    
+    printf("\nEnter a floating point number : ");
+    scanf("%f", &f);
+    
+    printf("\n%d ", num);
+    printf("\n%f ", f);
+    
+    return 0;
+}
